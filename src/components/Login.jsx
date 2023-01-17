e6afd70508f9d0bc362ad62604db1676425a535b
blackgirlbytes <blackgirlbytes@github.com>
2023-01-17 05:25:19 -0500

    auth adding

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,39 +3,23 @@ import { CTABanner, Button } from "@primer/react-brand";
 
 export default function Login() {
   const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <CTABanner hasShadow={false} align="center" hasBackground={false}>
+        <CTABanner.Heading>
+          Join GitHub’s Maintainer Community!
+        </CTABanner.Heading>
+        <CTABanner.Description>
+          The Maintainer Community is a private space for maintainers to connect
+          with peers, preview GitHub features, and help us support the open
+          source community.
+        </CTABanner.Description>
+        <CTABanner.ButtonGroup>
+          <Button onClick={() => console.log("Clicked!")}>
+            {"Login with GitHub"}
+          </Button>
+        </CTABanner.ButtonGroup>
+      </CTABanner>
     </>
-  )
-
-//   return (
-//     <>
-//       <CTABanner hasShadow={false} align="center" hasBackground={false}>
-//         <CTABanner.Heading>
-//           Join GitHub’s Maintainer Community!
-//         </CTABanner.Heading>
-//         <CTABanner.Description>
-//           The Maintainer Community is a private space for maintainers to connect
-//           with peers, preview GitHub features, and help us support the open
-//           source community.
-//         </CTABanner.Description>
-//         <CTABanner.ButtonGroup>
-//           <Button onClick={() => console.log("Clicked!")}>
-//             {"Login with GitHub"}
-//           </Button>
-//         </CTABanner.ButtonGroup>
-//       </CTABanner>
-//     </>
-//  );
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import Login from '../components/Login'
+import {
+  signIn,
+  getProviders,
+  useSession,
+} from "next-auth/react";
 
-export default function Home() {
+export default function Home({ providers }) {
+  const { data: session } = useSession();
   return (
     <>
       <Head>
@@ -13,7 +16,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Login />
+      {providers &&
+        Object.values(providers).map((provider) => (
+          <div key={provider.name} style={{ marginBottom: 0 }}>
+            <Login
+              session={session}
+              signIn={signIn}
+              provider={provider}
+            />
+          </div>
+        ))}
     </>
   )
+}
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
 }
