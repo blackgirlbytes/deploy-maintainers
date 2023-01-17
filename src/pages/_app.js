9e38509d4a7a29bf0a5531a2e077129b6f05c0f3
blackgirlbytes <blackgirlbytes@github.com>
2023-01-17 05:00:23 -0500

    session provider in app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,16 @@
 import "../styles/globals.css";
 import '@primer/react-brand/lib/css/main.css'
 import { BaseStyles,ThemeProvider, SSRProvider } from "@primer/react";
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps: {...pageProps } }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <ThemeProvider preventSSRMismatch>
       <SSRProvider>
         <BaseStyles>
-        
+          <SessionProvider session={session}>
             <Component {...pageProps} />
-
+          </SessionProvider>
         </BaseStyles>
       </SSRProvider>
     </ThemeProvider>
