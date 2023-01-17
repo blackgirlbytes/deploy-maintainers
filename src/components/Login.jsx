8152a8467a3fcaa88f691ac97f7c316e86bbb44c
blackgirlbytes <blackgirlbytes@github.com>
2023-01-17 05:08:30 -0500

    adding oauth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,41 @@
-import { CTABanner,Button } from "@primer/react-brand";
+import { useSession, signIn, signOut } from "next-auth/react"
+import { CTABanner, Button } from "@primer/react-brand";
 
 export default function Login() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
     <>
-      <CTABanner hasShadow={false} align="center" hasBackground={false}>
-        <CTABanner.Heading>
-          Join GitHub’s Maintainer Community!
-        </CTABanner.Heading>
-        <CTABanner.Description>
-          The Maintainer Community is a private space for maintainers to connect
-          with peers, preview GitHub features, and help us support the open
-          source community.
-        </CTABanner.Description>
-        <CTABanner.ButtonGroup>
-          <Button onClick={()=>console.log("Clicked!")} >
-            {"Login with GitHub"}
-          </Button>
-        </CTABanner.ButtonGroup>
-      </CTABanner>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
     </>
-  );
+  )
+
+//   return (
+//     <>
+//       <CTABanner hasShadow={false} align="center" hasBackground={false}>
+//         <CTABanner.Heading>
+//           Join GitHub’s Maintainer Community!
+//         </CTABanner.Heading>
+//         <CTABanner.Description>
+//           The Maintainer Community is a private space for maintainers to connect
+//           with peers, preview GitHub features, and help us support the open
+//           source community.
+//         </CTABanner.Description>
+//         <CTABanner.ButtonGroup>
+//           <Button onClick={() => console.log("Clicked!")}>
+//             {"Login with GitHub"}
+//           </Button>
+//         </CTABanner.ButtonGroup>
+//       </CTABanner>
+//     </>
+//  );
 }
