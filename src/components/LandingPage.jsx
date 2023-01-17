761cc4cb85f1695c43a275559ed59b6a0c37758a
blackgirlbytes <blackgirlbytes@github.com>
2023-01-17 06:31:03 -0500

    adding divs

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,15 +4,15 @@ export default function LandingPage({ signIn, provider, session }) {
   return (
     <>
       {!session ? (
-        <>
+        <div>
         not logged in
         <Login session={session} signIn={signIn} provider={provider} />
-        </>
+        </div>
       ) : (
-        <>
+        <div>
           logged in
           <button>log out </button>
-        </>
+        </div>
       )}
     </>
   );
